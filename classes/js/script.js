91f5bc8942b67e8b6cbb4f2c668c146bd37e5b94
AndrewVakaryuk <andreyvakaryuk@gmail.com>
2021-04-10 17:02:12 +0300

    class in JS

---
+++ classes/js/script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
+  }
+
+  calcArea() {
+    return this.height * this.width;
+  }
+}
+
+class ColoredRectangleWithText extends Rectangle {
+  constructor(height, width, text, bgColor) {
+    super(height, width);
+    this.text = text;
+    this.bgColor = bgColor;
+  }
+
+  showMyProps() {
+    console.log(`text: ${this.text}, color: ${this.bgColor}.`);
+  }
+}
+
+const div = new ColoredRectangleWithText(25, 10, 'Hello World', 'Red');
+div.showMyProps();
+console.log(div.calcArea());
+
+// const square = new Rectangle(10, 10);
+
+// console.log(square.calcArea());
